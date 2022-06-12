46171358c550aba64d94a221c2aeba3ee8a2c2ef
PriyeshGhosh <Priyeshghosh1997@gmail.com>
2022-06-12 12:14:16 +0530

    Calculator

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'dart:math';
+
+import 'package:calculator/buttons.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final myTextStyle = TextStyle(fontSize: 30, color: Colors.deepPurple[900]);
+  final List<String> buttons = [
+    'C',
+    'Del',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'X',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4),
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == 0) {
+                        return MyButton(
+                            buttonText: buttons[index],
+                            color: Colors.green,
+                            textcolor: Colors.white);
+                      } else if (index == 1) {
+                        return MyButton(
+                            buttonText: buttons[index],
+                            color: Colors.red,
+                            textcolor: Colors.white);
+                      } else {
+                        return MyButton(
+                          buttonText: buttons[index],
+                          color: isOperator(buttons[index])
+                              ? Colors.deepPurple
+                              : Colors.deepPurple[50],
+                          textcolor: isOperator(buttons[index])
+                              ? Colors.yellow
+                              : Colors.deepPurple,
+                        );
+                      }
+                    })),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'X' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+}
